3c0b2f3ce165ade7fdecc7326bc7bc422bb6f548
ACDAVID2 <ardeldavid2401@gmail.com>
2025-03-20 17:22:05 +0800

    Importing Part 1
    Imported assets from local to repo
    
    Asset Folder
    
    rearranged Assets

--- RPGGame/Assets/Scripts/PlayerControl.cs
+++ RPGGame/Assets/Scripts/PlayerControl.cs
@@ -4,38 +4,50 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public GameObject InteractIcon;
+    private GameObject InteractIcon;//used for clue interaction
     // Start is called before the first frame update
     public float movespd;
-    public float spdX;
-    public float spdY;
-    Rigidbody2D pl;
+    public float jump;
+    private float spdX;//movex
+    public Rigidbody2D pl;
     private GameObject ActiveUI;
+    public bool IsJumping;
 
     private Vector2 boxsize = new Vector2(0.1f, 1f);
     void Start()
     {
-        pl = GetComponent<Rigidbody2D>();//gravity
+        //pl = GetComponent<Rigidbody2D>();//gravity
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))//interact button
         {
             CheckInteraction();
         }
 
         if (Input.GetKeyDown(KeyCode.Escape)) 
         { 
-            if(ActiveUI != null)//if ui is active
+            if(ActiveUI != null)//checks if ui is active
             {
                 ActiveUI.SetActive(false);
             }
         }
-        spdX = Input.GetAxisRaw("Horizontal") * movespd;
-        spdY = Input.GetAxisRaw("Vertical") * movespd;
-        pl.velocity = new Vector2(spdX, spdY);
+        //spdX = Input.GetAxisRaw("Horizontal") * movespd;
+        //spdY = Input.GetAxisRaw("Vertical") * movespd;
+        //  pl.velocity = new Vector2(spdX, spdY);
+
+        spdX = Input.GetAxis("Horizontal");
+        pl.velocity = new Vector2(movespd * spdX, pl.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsJumping == false)
+        {
+            pl.AddForce(new Vector2(pl.velocity.x, jump));
+
+            
+        }
+
     }
 
     public void OpenInteractableIcon()
@@ -74,4 +86,25 @@ public class PlayerControl : MonoBehaviour
         }
         
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+     
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            IsJumping = false;
+        }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            IsJumping = true;
+        }
+
+    }
+
 }
